50231ba855dfb89327dc9afa3b2632a5578483ab
Sanchez201800 <Curette.tyra95@gmail.com>
2024-08-22 14:30:24 -0500

    Delete Project 6-3.java

--- Project 6-3.java
+++
@@ -1,8 +0,0 @@
-# Open my_data.txt file in read mode using 'with' statement
-with open('my_data.txt', 'r') as file_in:
-    # Open my_copy.txt file in write mode using 'with' statement
-    with open('my_copy.txt', 'w') as file_out:
-        # Iterate over each line in my_data.txt
-        for line in file_in:
-            # Write each line from my_data.txt to my_copy.txt
-            file_out.write(line)
